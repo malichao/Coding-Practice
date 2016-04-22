18934d99cfeaef24d966ece27ab3a5a1f11721ce
Malcolm Ma <malichaooo@gmail.com>
2016-04-22 01:51:13 -0500

    Fix error in linkedlist.cpp

--- linkedList.cpp
+++ linkedList.cpp
@@ -5,6 +5,11 @@
 #include <random>
 #include <algorithm>
 
+using std::cout;
+using std::endl;
+using std::vector;
+using std::shared_ptr;
+
 template <typename T>
 class Node{
 public:
@@ -46,26 +51,34 @@ node_ptr<T> mergeSortedLinkedList(node_ptr<T> F,node_ptr<T> L){
 }
 
 int main(){
+
 	srand(time(NULL));
-	std::vector<Node<int> > nodes1(10),nodes2(10);
-	for(size_t i=0;i<nodes1.size()-1;i++){
-		nodes1[i].data=rand()%20;
-		nodes2[i].data=rand()%20;
+	// Error! Don't define the data on the stack when using shared_ptr!
+	// Create data on the heap!
+	vector<Node<int> > *nodes1=new vector<Node<int> >(10);
+	vector<Node<int> > *nodes2=new vector<Node<int> >(10);
+	for(size_t i=0;i<(*nodes1).size();i++){
+		(*nodes1)[i].data=rand()%20;
+		(*nodes2)[i].data=rand()%20;
 	}
-	std::sort(nodes1.begin(),nodes1.end(),Node<int>::less);
-	std::sort(nodes2.begin(),nodes2.end(),Node<int>::less);
+	std::sort((*nodes1).begin(),(*nodes1).end(),Node<int>::less);
+	std::sort((*nodes2).begin(),(*nodes2).end(),Node<int>::less);
 
+	Node<int>* ptr1=(*nodes1).data();
+	for(size_t i=0;i<(*nodes1).size()-1;i++){
+		(*nodes1)[i].next=node_ptr<int> (ptr1+i+1);
+		(*nodes2)[i].data=rand()%20;
+	}
 
-	nodes1.back().next=nullptr;
-	nodes1.back().next=nullptr;
+	(*nodes1).back().next=nullptr;
+	(*nodes1).back().next=nullptr;
 
-	std::cout<<"Node List1: ";
-	for(auto n:nodes1)
-		std::cout<<n.data<<" ";
-	std::cout<<"\nNode List2: ";
-	for(auto n:nodes2)
-		std::cout<<n.data<<" ";
-	std::cout<<"\nMerged List: ";
-	//for(auto n:nodes2)
-	//	std::cout<<n.data<<" ";
+	node_ptr<int> nodeHead1=node_ptr<int>((*nodes1).data());
+	cout<<"Node List1: ";
+	while(nodeHead1!=nullptr){
+		cout<<nodeHead1->data<<" ";
+		nodeHead1=nodeHead1->next;
+	}
+	cout<<"\nNode List 2:";
+
 }
