4becdc4d2a4e07eed84841cb980df068ba78d53a
Malcolm Ma <malichaooo@gmail.com>
2016-04-24 13:54:38 -0500

    Fix bug in malloc_2D_array

--- memory_management/malloc_2D_array.c
+++ memory_management/malloc_2D_array.c
@@ -9,37 +9,43 @@ void** malloc_2D_array(size_t row,size_t col,size_t size){
 }
 
 void** malloc_2D_array1(size_t row,size_t col,size_t size){
-	void** addr=(void**) malloc(row*size + row*col*size);
-	void* array=addr+row*size;
+	void** addr=(void**) malloc(row*sizeof(void*) + row*col*size);
+	void* array=&addr[row];
 	for(size_t i=0;i<row;i++)
 		addr[i]=(void*)(i*(col*size)+(size_t)array);
 	return addr;
 }
 
-
+#define TYPE double
+#define PRINT_FORMAT "%f \t"
+#define VALUE j
 int main(){
 	size_t row=5,col=5;
+
 	printf("Method 1\n");
-	int** a=(int**)malloc_2D_array(row,col,sizeof(int));
+	TYPE** a=(TYPE**)malloc_2D_array(row,col,sizeof(TYPE));
 	for(int i=0;i<row;i++)
 		for(int j=0;j<col;j++)
-			a[i][j]=j;
+			a[i][j]=VALUE;
 	for(int i=0;i<row;i++){
 		for(int j=0;j<col;j++)
-			printf("%d\t",a[i][j]);
+			printf(PRINT_FORMAT,a[i][j]);
 		printf("\n");
 	}
 	free(a);
 
+
 	printf("Method 2\n");
-	int** b=(int**)malloc_2D_array1(row,col,sizeof(int));
+	TYPE** b=(TYPE**)malloc_2D_array1(row,col,sizeof(TYPE));
 	for(int i=0;i<row;i++)
 		for(int j=0;j<col;j++)
-			b[i][j]=j;
+			b[i][j]=VALUE;
+
 	for(int i=0;i<row;i++){
 		for(int j=0;j<col;j++)
-			printf("%d\t",b[i][j]);
+			printf(PRINT_FORMAT,b[i][j]);
 		printf("\n");
 	}
 	free(b);
+
 }
