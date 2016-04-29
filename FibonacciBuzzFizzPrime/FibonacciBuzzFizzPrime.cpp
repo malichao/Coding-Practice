a18a2d4d6ea3b2aedaac4307e613abb585352cc1
Malcolm Ma <malichaooo@gmail.com>
2016-04-28 23:59:15 -0500

    create class

--- FibonacciBuzzFizzPrime/FibonacciBuzzFizzPrime.cpp
+++
@@ -1,8 +0,0 @@
-/*
- * FibonacciBuzzFizzPrime.cpp
- *
- *  Created on: Apr 28, 2016
- *      Author: Lichao
- */
-
-
---
+++ FibonacciBuzzFizzPrime/FibonacciBuzzFizzPrime.h
@@ -0,0 +1,28 @@
+/*
+ * FibonacciBuzzFizzPrime.cpp
+ *
+ *  Created on: Apr 28, 2016
+ *      Author: Lichao
+ */
+#include <utility>
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+class FBFP{
+public:
+	typedef std::pair<uint64_t,string> fbfp_pair;
+
+	//generate fibonacci series and its regarding string for n
+	//===Test Cases===
+	//-null cases	 : n=0,result.size()=0
+	//-illegal cases : n<0
+	//-range cases	 : fibonacci >=uint64_t
+	//-normal cases	 : n=1,2,3,4,5,...
+	void generate(const int n,vector<fbfp_pair> &result){
+
+	}
+
+};
+
